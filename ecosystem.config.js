c0c4c4829fb4b1fe1007bb064072a451f68ee8e3
AleonaKS <challengesim@gmail.com>
2026-01-25 16:38:06 +0400

    Подготовка к деплою

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -36,7 +36,8 @@ module.exports = {
       repo: 'git@github.com:AleonaKS/nodejs-pm2-deploy.git',
       path: '/home/user/nodejs-pm2-deploy',
       'pre-deploy': 'git fetch --all',
-      'post-deploy': 'cd backend && npm run build && cd .. && npm install --omit=dev && pm2 reload ecosystem.config.js --env production'
+      'post-deploy': 'cd backend && npm ci && npm run build && cd .. && pm2 reload ecosystem.config.js --env production'
+
     }
   }
 };
