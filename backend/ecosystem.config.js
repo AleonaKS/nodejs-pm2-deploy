76caa5b7989331079354d9a2c39995a3920f50be
AleonaKS <challengesim@gmail.com>
2026-01-30 15:04:46 +0400

    Add ecosystem.config.js and .env.deploy.example for pm2 deployment

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,25 +1,27 @@
 module.exports = {
   apps: [{
-    name: 'frontend',
-    script: 'npx',
-    args: 'serve -s build',
+    name: 'backend',
+    script: './dist/app.js',  
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
     env_production: {
       NODE_ENV: 'production'
-    },
-    deploy: {
-      production: {
-        user: process.env.DEPLOY_USER,
-        host: process.env.DEPLOY_HOST,
-        ref: process.env.DEPLOY_BRANCH,
-        repo: process.env.DEPLOY_REPO,
-        path: process.env.DEPLOY_PATH,
-        'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
-      }
     }
-  }]
+  } ],
+  deploy: {
+    production: {
+      user: 'user',
+      host: '158.160.210.113',
+      ref: 'origin/master',
+      repo: 'git@github.com:AleonaKS/nodejs-pm2-deploy.git',
+      path: '/home/user/nodejs-pm2-deploy',
+      'pre-deploy': 'git fetch --all',
+      'post-deploy': 'cp /home/user/nodejs-pm2-deploy/backend/.env ./backend/.env && export NODE_OPTIONS=--openssl-legacy-provider && npm install && cd backend && npm install && npm run build && cd ../frontend && npm install && npm run build && cd .. && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 };
-
