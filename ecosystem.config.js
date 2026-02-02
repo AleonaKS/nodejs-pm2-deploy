ad2edb77e95f5f9884a8286f6d0b5755743eb70e
AleonaKS <challengesim@gmail.com>
2026-02-02 06:17:07 +0000

    feat: update ecosystem.config.js for pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,52 @@
+// ecosystem.config.js
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER,
+  DEPLOY_HOST,
+  DEPLOY_PATH,
+  DEPLOY_REF = 'main',
+  DEPLOY_REPO,
+} = process.env;
+
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      script: './backend/dist/app.js',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'frontend',
+      script: 'npx serve',
+      args: '-s -l 3001 ./frontend/dist',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH,
+      'pre-deploy': 'scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env',
+      'post-deploy': 'cd $DEPLOY_PATH && npm install && cd backend && npm run build && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+};
