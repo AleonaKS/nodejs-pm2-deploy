bf0811e4fa0ee4af6c43dc72ef95fc6829649336
AleonaKS <challengesim@gmail.com>
2026-02-04 17:48:44 +0400

    fix: remove dotenv from ecosystem.config.js, add env_file for server .env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,39 @@
 module.exports = {
   apps: [{
     name: 'nodejs-app',
-    script: './backend/dist/app.js',
-    user: process.env.DEPLOY_USER,      
-    cwd: process.env.DEPLOY_PATH, 
+    script: './dist/app.js',  
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    
+    
     env: {
       NODE_ENV: 'development',
+      PORT: 3000,
       DB_ADDRESS: process.env.DB_ADDRESS,    
       JWT_SECRET: process.env.JWT_SECRET,   
     },
     env_production: {
       NODE_ENV: 'production',
-      PORT: 80,
+      PORT: process.env.PORT,
       DB_ADDRESS: process.env.DB_ADDRESS,
       JWT_SECRET: process.env.JWT_SECRET,
-    },
-    deploy: {
-      ref: process.env.DEPLOY_REF,          
-      repo: process.env.DEPLOY_REPO,      
-      key: process.env.DEPLOY_KEY,    
+    }
+  }],
+  
+ 
+  deploy: {
+    production: {
+      user: process.env.DEPLOY_USER,
+      host: process.env.DEPLOY_HOST,
+      ref: process.env.DEPLOY_REF,
+      repo: process.env.DEPLOY_REPO,
+      path: process.env.DEPLOY_PATH,
+      key: process.env.DEPLOY_KEY,
+      'pre-deploy': 'git fetch --all',
       'post-deploy': 'npm install && npm run build && pm2 reload ecosystem.config.js --env production'
     }
-  }]
+  }
 };
 
