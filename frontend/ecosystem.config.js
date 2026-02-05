aa3112d3689ba53cbeeccb8529b06d36dd624b3a
AleonaKS <challengesim@gmail.com>
2026-02-05 22:35:06 +0400

    fix

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,16 @@
+require('dotenv').config({ path: './.env.deploy' }); 
+
+const {  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
+
+module.exports = { 
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPO,
+      path: DEPLOY_PATH, 
+      'post-deploy': 'cd frontend && pwd && npm ci && npm i && npm run build && pm2 startOrRestart ecosystem.config.js -- env production',
+    },
+  },
+};
