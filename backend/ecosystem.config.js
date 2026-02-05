fea6e33eab9bbc426a866532c8776159b4a64120
AleonaKS <challengesim@gmail.com>
2026-02-05 20:52:08 +0400

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -2,13 +2,6 @@ require('dotenv').config({ path: './.env.deploy' });
 
 const {  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF, DEPLOY_REPO } = process.env;
 
-// Проверка — для отладки (можно удалить после успеха)
-console.log('✅ DEPLOY_USER:', DEPLOY_USER);
-console.log('✅ DEPLOY_HOST:', DEPLOY_HOST);
-console.log('✅ DEPLOY_PATH:', DEPLOY_PATH);
-console.log('✅ DEPLOY_REF:', DEPLOY_REF);
-console.log('✅ DEPLOY_REPO:', DEPLOY_REPO);
-
 module.exports = {
   apps: [{
     name: 'mesto',
